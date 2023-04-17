77cf3b9489c4e496a1ec2166fb200dbcca3d0c35
angeelams <130452816+angeelams@users.noreply.github.com>
2023-04-17 09:37:59 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,28 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Giraffe)
-    basic.pause(1000)
-    basic.showIcon(IconNames.Happy)
-    basic.pause(1000)
-})
-basic.forever(function () {
-	
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # # . . .
+            . # . . .
+            . # . . .
+            . # # # .
+            . # . # .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            # # . . .
+            # # . # #
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+        basic.pause(1000)
+    }
 })
