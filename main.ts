3e2d4a789ec6d8ef3adb15290c487dbfb1a1a49e
angeelams <130452816+angeelams@users.noreply.github.com>
2023-04-17 09:42:42 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,3 +26,31 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(1000)
     }
 })
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            # # . # #
+            # # # # #
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(1000)
+    }
+})
