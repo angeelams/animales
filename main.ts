c696f1029b21cc3d7a339f8e7627c2a753390095
angeelams <130452816+angeelams@users.noreply.github.com>
2023-04-17 09:33:12 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Giraffe)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+})
+basic.forever(function () {
+	
+})
